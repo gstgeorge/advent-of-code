9d3690a1c962d841667bcf0ca2466b8ff9db31bd
Gavin St. George <gavinstgeorge@outlook.com>
2025-12-07 01:33:07 -0600

    day 02

---
+++ 2025/02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+string[] data = File.ReadAllLines("data.txt")[0].Split(",");
+
+Regex reA = new(@"^((?:[1-9]){1}(?:\d)*)\1$");
+Regex reB = new(@"^((?:[1-9]){1}(?:\d)*)\1+$");
+
+long totalA = 0;
+long totalB = 0;
+
+foreach (var record in data)
+{
+    var range = record.Split("-");
+
+    for (long i = long.Parse(range[0]); i <= long.Parse(range[1]); i++)
+    {
+        if (reA.IsMatch(i.ToString()))
+        {
+            totalA += i;
+        }
+
+        if (reB.IsMatch(i.ToString()))
+        {
+            totalB += i;
+        }
+    }
+}
+
+Console.WriteLine($"Part A: {totalA}");
+Console.WriteLine($"Part B: {totalB}");
